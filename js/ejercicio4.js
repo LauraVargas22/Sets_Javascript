be5a6a541ff7b9d17478b1a11dad08d415a78430
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-01-10 16:52:22 -0500

    feat: :sparkles: Fourth Exercise Find common elements between two arrays

---
+++ js/ejercicio4.js
@@ -0,0 +1,12 @@
+/* Intersección de conjuntos
+   By Mariana Vargas 
+*/
+
+//Arreglos proporcionados convertidos a sets
+const conjuntoA = new Set([1, 2, 3, 4, 5]);
+const conjuntoB = new Set([4, 5, 6, 7, 8]);
+
+//Uso de intersección para conocer los elementos comunes del conjunto A y B
+const elementosComunes = conjuntoA.intersection(conjuntoB);
+//Impresión de los elementos comunes
+console.log({elementosComunes});
