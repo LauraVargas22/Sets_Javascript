f793151cc3f2cf1b1ac9ad3a252db20db8a4de10
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-01-10 16:21:34 -0500

    feat: :tada: First exercise Create a set and show the number of elements in it.

---
+++ js/ejercicio1.js
@@ -0,0 +1,11 @@
+/* Creación de set con números del 1 al 10. 
+   Imprimiendo su contenido y verificando su tamaño
+   By Mariana Vargas 
+*/
+
+//Creación del Set
+const set1 = new Set([1,2,3,4,5,6,7,8,9,10]);
+//Impresión en consola del set
+console.log(set1);
+//Se imprime en consola el número de elementos en el set
+console.log(`El número de elementos del set es ${(set1.size)}`)
